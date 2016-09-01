a22d996356b5241623caedd2b0d3625eb18fb446
nukui-s <nukui.s.aa@gmail.com>
2016-09-01 15:23:02 +0900

    make procedures functions

--- tripletembed/model.py
+++ tripletembed/model.py
@@ -18,8 +18,8 @@ class TripletEmbedding(object):
     Tripet Embedding model
     """
 
-    def __init__(self, n_entity, n_relation, entity_factors, relation_factors,
-                 hidden_units, lambda_w=10e-3, sigma=1.0, activation_func="relu",
+    def __init__(self, n_entity=None, n_relation=None, entity_factors=None, relation_factors=None,
+                 hidden_units=None, lambda_w=10e-3, sigma=1.0, activation_func="relu",
                  optimizer="adam", learning_rate=10e-2, w_regularization=10e-4,
                  n_negative_sample=5):
         """
@@ -40,21 +40,40 @@ class TripletEmbedding(object):
         if not hasattr(tf.train, OPTIMIZER_NAME[optimizer]):
             raise("TensorFlow has no such optimizer {}".format(optimizer))
 
-        self.n_entity = n_entity
-        self.n_relation = n_relation
-        self.hidden_units = hidden_units
-        self.w_regularization = w_regularization
-        self.sigma = sigma
-        self.learning_rate = learning_rate
-        self.entity_factors = entity_factors
-        self.relation_factors = relation_factors
-        self.activation_func = activation_func
-        self.optimizer = OPTIMIZER_NAME[optimizer]
-        self.n_negative_sample = n_negative_sample
-        self.build()
+        self.config = config = {}
+        config["n_entity"] = n_entity
+        config["n_relation"] = n_relation
+        config["hidden_units"] = hidden_units
+        config["w_regularization"] = w_regularization
+        config["sigma"] = sigma
+        config["learning_rate"] = learning_rate
+        config["entity_factors"] = entity_factors
+        config["relation_factors"] = relation_factors
+        config["activation_func"] = activation_func
+        config["optimizer"] = optimizer
+        config["n_negative_sample"] = n_negative_sample
+        # create alias for attributes
+        for k, v  in config.items():
+            setattr(self, k, v)
+        if (self.n_entity is not None and self.n_relation is not None and
+            self.entity_factors is not None and self.relation_factors is not None and
+            self.hidden_units is not None):
+            self.build()
+            self.ready = True
+        else:
+            self.ready = False
 
     def fit(self, subjects, objects, predicates, batch_size=128,
             nb_epoch=10, logdir="log"):
+        if not self.ready:
+            if (self.n_entity is not None and self.n_relation is not None and
+                self.entity_factors is not None and self.relation_factors is not None and
+                self.hidden_units is not None):
+                self.build()
+                self.ready = True
+            else:
+                raise AttributeError("You must specify the architecture of the model")
+
         subjects = np.array(subjects, dtype=np.int64)
         objects = np.array(objects, dtype=np.int64)
         predicates = np.array(predicates, dtype=np.int64)
@@ -110,93 +129,36 @@ class TripletEmbedding(object):
         predicate_all = np.append(predicates, predicate_neg)
         return subjects_all, objects_all, predicate_all, labels
 
+    def _get_entity_embed_variable(self):
+        entity_scale = 1.0 / np.sqrt(self.entity_factors)
+        entity_embed = tf.get_variable(name="entity_embed",
+                                       shape=[self.n_entity, self.entity_factors],
+                                       dtype=tf.float32,
+                                       initializer=tf.random_uniform_initializer(
+                                       minval= -entity_scale,
+                                       maxval= entity_scale))
+        tf.histogram_summary("entity_embed", self._entity_embed)
+        return entity_embed
 
-    def build(self):
-        """
-        build computation graph
-        """
-        self.graph = tf.Graph()
-        with self.graph.as_default():
+    def _get_relation_embed_variable(self):
+        relation_scale = 1.0 / np.sqrt(self.relation_factors) 
+        relation_embed = tf.get_variable(name="relation_embed",
+                                         shape=[self.n_relation, self.relation_factors],
+                                         dtype=tf.float32,
+                                         initializer=tf.random_uniform_initializer(
+                                         minval= -relation_scale,
+                                         maxval = relation_scale))
+        tf.histogram_summary("relation_embed", self._relation_embed)
+        return relation_embed
 
-            # trainable variables
-            entity_scale = 1.0 / np.sqrt(self.entity_factors)
-            relation_scale = 1.0 / np.sqrt(self.relation_factors)
-
-            self._entity_embed = tf.get_variable(name="entity_embed",
-                                        shape=[self.n_entity, self.entity_factors],
-                                        dtype=tf.float32,
-                                        initializer=tf.random_uniform_initializer(
-                                            minval= -entity_scale,
-                                            maxval= entity_scale))
-            self._relation_embed = tf.get_variable(name="relation_embed",
-                                                  shape=[self.n_relation, self.relation_factors],
-                                                  dtype=tf.float32,
-                                                  initializer=tf.random_uniform_initializer(
-                                                      minval= -relation_scale,
-                                                      maxval = relation_scale))
-            tf.histogram_summary("entity_embed", self._entity_embed)
-            tf.histogram_summary("relation_embed", self._relation_embed)
-
-            # input nodes
-            self._subjects = tf.placeholder(tf.int64, shape=[None], name="subjects")
-            self._objects = tf.placeholder(tf.int64, shape=[None], name="objects")
-            self._predicates = tf.placeholder(tf.int64, shape=[None], name="predicates")
-            self._exist_labels = tf.placeholder(tf.float32, shape=[None], name="exist_labels")
-
-            # look up embedding vectors
-            subject_embed = tf.nn.embedding_lookup(self._entity_embed, self._subjects)
-            object_embed = tf.nn.embedding_lookup(self._entity_embed, self._objects)
-            predicate_embed = tf.nn.embedding_lookup(self._relation_embed, self._predicates)
-
-            # concatenate subject and object embeddings
-            concat_embed = tf.concat(1, [subject_embed, object_embed])
-
-            # estimated relation embeddings
-            self._estimated_embed = self.estimate_relation_embeddings(concat_embed)
-
-            # l2 loss between relation embeddings
-            l2_loss = tf.reduce_sum(tf.pow(predicate_embed - self._estimated_embed, 2),
-                                    reduction_indices=1)
-
-            reg_term = tf.nn.l2_loss(subject_embed)
-            reg_term += tf.nn.l2_loss(object_embed)
-            reg_term += tf.nn.l2_loss(predicate_embed)
-            for w in self._weights:
-                reg_term += tf.nn.l2_loss(w)
-            for b in self._biases:
-                reg_term += tf.nn.l2_loss(b)
-
-            tf.histogram_summary("l2_loss", l2_loss)
-
-            self.proba = proba = tf.exp(-self.sigma * l2_loss)
-            loss = tf.reduce_sum(self.sigma * l2_loss - \
-                (1/2) * (1 - self._exist_labels) * tf.log(tf.exp(self.sigma * l2_loss) - 1 + 10e-6))
-
-            self.cost = loss + self.w_regularization * reg_term
-
-            tf.histogram_summary("proba", proba)
-
-            tf.scalar_summary("loss", loss)
-            tf.scalar_summary("regularizer", reg_term)
-            tf.scalar_summary("regularizer_scaled", reg_term*self.w_regularization)
-            tf.scalar_summary("cost", self.cost)
-
-            self.summaries = tf.merge_all_summaries()
-
-            optimizer = getattr(tf.train, self.optimizer)
-            self.opt = optimizer(self.learning_rate).minimize(self.cost)
-
-            self.sess = tf.Session()
-            self.init_op = tf.initialize_all_variables()
-            self.sess.run(self.init_op)
-
-
-    def estimate_relation_embeddings(self, concat_embed):
+    def _get_relational_latent_factor(self, subject_embed, object_embed):
         """
         estimate relation embeddings between subjects and objects
         :param concat_embed: concatenated embeddings
         :return: relation embeddings
         """
+        # concatenate subject and object embeddings
+        concat_embed = tf.concat(1, [subject_embed, object_embed])
         activation_func = getattr(tf.nn, self.activation_func)
         self._weights = []
         self._biases = []
@@ -220,6 +182,92 @@ class TripletEmbedding(object):
         return output
 
 
+    def build(self):
+        """
+        build computation graph
+        """
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+
+            # trainable variables
+            self._entity_embed = self._get_entity_embed_variable()
+            self._relation_embed = self._get_relation_embed_variable()
+
+
+            # input nodes
+            self._subjects = tf.placeholder(tf.int64, shape=[None], name="subjects")
+            self._objects = tf.placeholder(tf.int64, shape=[None], name="objects")
+            self._predicates = tf.placeholder(tf.int64, shape=[None], name="predicates")
+            self._exist_labels = tf.placeholder(tf.float32, shape=[None], name="exist_labels")
+
+            # look up embedding vectors
+            subject_embed = tf.nn.embedding_lookup(self._entity_embed, self._subjects)
+            object_embed = tf.nn.embedding_lookup(self._entity_embed, self._objects)
+            predicate_embed = tf.nn.embedding_lookup(self._relation_embed, self._predicates)
+
+            # estimated relation embeddings
+            relational_embed = self._get_relational_latent_factor(subject_embed, object_embed)
+
+            # l2 loss between relation embeddings
+            l2_loss = tf.reduce_sum(tf.pow(predicate_embed - relational_embed, 2),
+                                    reduction_indices=1)
+            tf.histogram_summary("l2_loss", l2_loss)
+
+            reg_term = tf.nn.l2_loss(subject_embed)
+            reg_term += tf.nn.l2_loss(object_embed)
+            reg_term += tf.nn.l2_loss(predicate_embed)
+            for w in self._weights:
+                reg_term += tf.nn.l2_loss(w)
+            for b in self._biases:
+                reg_term += tf.nn.l2_loss(b)
+
+
+            #self.proba = proba = tf.exp(-self.sigma * l2_loss)
+            #loss = tf.reduce_sum(self.sigma * l2_loss - \
+            #    (1/2) * (1 - self._exist_labels) * tf.log(tf.exp(self.sigma * l2_loss) - 1 + 10e-6))
+
+            loss = tf.reduce_sum(self._exist_labels * l2_loss)
+
+            self.cost = (loss + self.w_regularization * reg_term)
+
+            tf.scalar_summary("loss", loss)
+            tf.scalar_summary("regularizer", reg_term)
+            tf.scalar_summary("regularizer_scaled", reg_term*self.w_regularization)
+            tf.scalar_summary("cost", self.cost)
+
+            self.summaries = tf.merge_all_summaries()
+
+            optimizer = getattr(tf.train, OPTIMIZER_NAME[self.optimizer])
+            self.opt = optimizer(self.learning_rate).minimize(self.cost)
+
+            self.sess = tf.Session()
+            self.init_op = tf.initialize_all_variables()
+            self.sess.run(self.init_op)
+
+
+    def build_assign_ops(self):
+        # Assign operation
+        self.input_entity_embed = tf.placeholder("float32", shape=self._entity_embed.get_shape())
+        self.assign_entity_embed = self._entity_embed.assign(self.input_entity_embed)
+        self.input_relation_embed = tf.placeholder("float32", shape=self._relation_embed.get_shape())
+        self.assign_relation_embed = self._relation_embed.assign(self.input_relation_embed)
+
+        self.input_weight = []
+        self.input_bias = []
+        self.assign_weight = []
+        self.assign_bias = []
+
+        for w in self._weights:
+            input_w = tf.placeholder("float32", shape=w.get_shape())
+            self.input_weight.append(input_w)
+            self.assign_weight.append(w.assign(input_w))
+
+        for b in self._biases:
+            input_b = tf.placeholder("float32", shape=b.get_shape())
+            self.input_bias.append(input_b)
+            self.assign_bias.append(b.assign(input_b))
+
+
     def get_entity_embedding(self, indices=None):
         entity_embed = self.sess.run(self._entity_embed)
         if indices:
@@ -228,13 +276,19 @@ class TripletEmbedding(object):
             return entity_embed
 
     def get_relation_embedding(self, indices=None):
-        relation_embed = self._relation_embed)
+        relation_embed = self.sess.run(self._relation_embed)
         if indices:
             return relation_embed[indices, :]
         else:
             return relation_embed
 
     def set_entity_embedding(self, entity_embed):
+        self.sess.run(self.assign_entity_embed,
+                      feed_dict={self.input_entity_embed: entity_embed})
+
+    def set_relation_embedding(self, relation_embed):
+        self.sess.run(self.assign_relation_embed,
+                      feed_dict={self.input_relation_embed: relation_embed})
 
     def get_estimated_embedding(self, subjects, objects):
         subjects = np.array(subjects, dtype=np.int64)
@@ -253,12 +307,20 @@ class TripletEmbedding(object):
         stored["relation_embed"] = relation_embed
         stored["weights"] = weights
         stored["biases"] = biases
-
+        stored["config"] = self.config
         joblib.dump(stored, save_file)
 
     @classmethod
     def restore(cls, save_file):
         stored = joblib.load(save_file)
+        config = stored["config"]
+        model = cls(**config)
+        model.set_entity_embedding(stored["entity_embed"])
+        model.set_relation_embedding(stored["relation_embed"])
+        model.set_weights(stored["weights"])
+        model.set_biases(stored["biases"])
+        return model
+
 
     def get_weights(self):
         return [self.sess.run(w) for w in self._weights]
@@ -269,14 +331,12 @@ class TripletEmbedding(object):
 
     def set_weights(self, weights):
         for i, weight_i in enumerate(weights):
-            if weight_i.shape != self._weights[i].shape:
+            if weight_i.shape != tuple(self._weights[i].get_shape().as_list()):
                 raise ValueError("The shape of weight in layer {}th is invalid".format(i))
-            self._weights[i] = weight_i
+            self.sess.run(self.assign_weight[i], {self.input_weight[i]: weight_i})
 
     def set_biases(self, biases):
-        for i, bias_i in enumerate(biases)
-            if bias_i.shape != self._biases[i].shape:
+        for i, bias_i in enumerate(biases):
+            if bias_i.shape != tuple(self._biases[i].get_shape().as_list()):
                 raise ValueError("The shape of bias in layer {}th is invalid".format(i))
-            self._biases[i] = bias_i
-
-
+            self.sess.run(self.assign_bias[i], {self.input_bias[i]: bias_i})
